e3b0c4e622b3ed7b61f4436d6df3e7fd3c60604e
cjloveqhy <15557422216@139.com>
2024-05-08 14:38:04 +0800

    feat: 添加接口拦截器

--- project/src/main/java/com/cby/tcs/config/WebMvcConfig.java
+++ project/src/main/java/com/cby/tcs/config/WebMvcConfig.java
@@ -1,11 +1,29 @@
 package com.cby.tcs.config;
 
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import com.freedom.cloud.config.AppConfigProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AppConfigProperties appConfigProperties;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        List<String> excludePath = appConfigProperties.getExcludePath();
+        registry.addInterceptor(new SaInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(excludePath == null ? new ArrayList<>() : excludePath);
+    }
 
     // 设置跨域访问
     @Override
